58fd664d6cfb8595983bdafcc499bce0262808a5
Lucas Nisbach <lucas@unkreativ.net>
2012-11-27 11:43:33 +0100

    Added Content for unregistered Users

--- lib/content.php
+++ lib/content.php
@@ -11,14 +11,49 @@ switch( $_GET["c"] ) {
 		echo '
 			<header><h1><span aria-hidden="true" class="icon-home"></span> Startseite</h1></header>
 			<article>
-				<!-- TODO: Text! -->
+				</br>
+				Herzlich willkommen beim DSA4-Assistenten, </br></br>
+
+				Hast du schon immer ein Programm gesucht um schnell und einfach deine DSA4_Charaktere zu verwalten?</br>
+				Egal ob es sich um reine Chat RGP Chars handelt oder richtige Pen & Paper Chars, hier kriegst du eine</br>
+				ordentliche Basis um diese zu verwalten. Du kannst alle Werte Eintragen, eine Beschreibung hinzufügen, </br>
+				Dinge weglassen und vieles mehr.</br></br>
+
+				Der DSA4-Assistent wird ständig ausgebaut! Wir testen neue Funktionen und wollen</br>
+				euch mehr Komfort bieten.</br></br>
+
+				Die Idee, des DSA4-Assistent stammt von Wolfsterror.</br>
+				Das Projekt wird realisiert durch die hervorragende Hilfe von <a href="http://pakldev.de" target="_blank">PakL</a>!</br>
+
+
 			</article>';
 		break;
 	case 1:
 		echo '
 			<header><h1><span aria-hidden="true" class="icon-user"></span> User</h1></header>
 			<article>
-				<!-- TODO: Text! -->
+				</br>
+				Hallo reisender,</br>
+				hier würdest du deine Benutzer verwaltung finden. Wobei wir uns noch nicht sicher damit sind </br>
+				Aber du bist nicht eingeloggt, deswegen ist es nicht möglich, dir diese Seite gänzlichst </br>
+				zu zeigen. Also schnell registrieren oder einloggen und die Welt des DSA4-Assistent erkunden</br>
+				</br>
+				Viel Spaß beim weiteren Stöbern</br>
+
+			</article>';
+		break;
+	case 2:		
+		echo '
+			<header><h1><span aria-hidden="true" class="icon-clipboard"></span> Charaktere</h1></header>
+			<article>
+				</br>
+				Hier findest du eine auflistung aller Charaktere, die von den Usern als öffentlich makiert wurden.</br>
+				Jeder Char hat seine eigene Seite, klicke einfach auf den Namen um dir einen Char genauer an zu schauen</br>
+				Wenn du eingeloggt bist, kriegst du sogar noch mehr Informationen. Denn neben der Liste die du hier siehst,</br>
+				bekommst du noch eine Liste deiner eigenen Chars und kannst diese modofizieren</br>
+				</br>
+				Also viel Spaß beim stöbern!</br>
+
 			</article>';
 		break;
 	case 3:
