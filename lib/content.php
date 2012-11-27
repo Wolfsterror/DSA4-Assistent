0270d457d0f5a8ed5378f59e9c47c5c91fae148b
Lucas Nisbach <lucas@unkreativ.net>
2012-11-27 13:27:57 +0100

    make content code nicer

--- lib/content.php
+++ lib/content.php
@@ -11,20 +11,19 @@ switch( $_GET["c"] ) {
 		echo '
 			<header><h1><span aria-hidden="true" class="icon-home"></span> Startseite</h1></header>
 			<article>
-				</br>
-				Herzlich willkommen beim DSA4-Assistenten, </br></br>
+				<p>
+				Herzlich willkommen beim DSA4-Assistenten, <p>
 
-				Hast du schon immer ein Programm gesucht um schnell und einfach deine DSA4_Charaktere zu verwalten?</br>
-				Egal ob es sich um reine Chat RGP Chars handelt oder richtige Pen & Paper Chars, hier kriegst du eine</br>
-				ordentliche Basis um diese zu verwalten. Du kannst alle Werte Eintragen, eine Beschreibung hinzufügen, </br>
-				Dinge weglassen und vieles mehr.</br></br>
+				Hast du schon immer ein Programm gesucht um schnell und einfach deine DSA4_Charaktere zu verwalten?<br />
+				Egal ob es sich um reine Chat RGP Chars handelt oder richtige Pen & Paper Chars, hier kriegst du eine<br />
+				ordentliche Basis um diese zu verwalten. Du kannst alle Werte Eintragen, eine Beschreibung hinzufügen,<br />
+				Dinge weglassen und vieles mehr.<p>
 
-				Der DSA4-Assistent wird ständig ausgebaut! Wir testen neue Funktionen und wollen</br>
-				euch mehr Komfort bieten.</br></br>
+				Der DSA4-Assistent wird ständig ausgebaut! Wir testen neue Funktionen und wollen<br />
+				euch mehr Komfort bieten.<p>
 
-				Die Idee, des DSA4-Assistent stammt von Wolfsterror.</br>
-				Das Projekt wird realisiert durch die hervorragende Hilfe von <a href="http://pakldev.de" target="_blank">PakL</a>!</br>
-
+				Die Idee, des DSA4-Assistent stammt von Wolfsterror.<br />
+				Das Projekt wird realisiert durch die hervorragende Hilfe von <a href="http://pakldev.de" target="_blank">PakL</a>!<br />
 
 			</article>';
 		break;
@@ -32,13 +31,13 @@ switch( $_GET["c"] ) {
 		echo '
 			<header><h1><span aria-hidden="true" class="icon-user"></span> User</h1></header>
 			<article>
-				</br>
-				Hallo reisender,</br>
-				hier würdest du deine Benutzer verwaltung finden. Wobei wir uns noch nicht sicher damit sind </br>
-				Aber du bist nicht eingeloggt, deswegen ist es nicht möglich, dir diese Seite gänzlichst </br>
-				zu zeigen. Also schnell registrieren oder einloggen und die Welt des DSA4-Assistent erkunden</br>
-				</br>
-				Viel Spaß beim weiteren Stöbern</br>
+				<br />
+				Hallo Reisender,<br />
+				hier würdest du deine Benutzer verwaltung finden. Wobei wir uns noch nicht sicher damit sind<br />
+				Aber du bist nicht eingeloggt, deswegen ist es nicht möglich, dir diese Seite gänzlichst <br />
+				zu zeigen. Also schnell registrieren oder einloggen und die Welt des DSA4-Assistent erkunden<p>
+				
+				Viel Spaß beim weiteren Stöbern<p>
 
 			</article>';
 		break;
@@ -46,13 +45,13 @@ switch( $_GET["c"] ) {
 		echo '
 			<header><h1><span aria-hidden="true" class="icon-clipboard"></span> Charaktere</h1></header>
 			<article>
-				</br>
-				Hier findest du eine auflistung aller Charaktere, die von den Usern als öffentlich makiert wurden.</br>
-				Jeder Char hat seine eigene Seite, klicke einfach auf den Namen um dir einen Char genauer an zu schauen</br>
-				Wenn du eingeloggt bist, kriegst du sogar noch mehr Informationen. Denn neben der Liste die du hier siehst,</br>
-				bekommst du noch eine Liste deiner eigenen Chars und kannst diese modofizieren</br>
-				</br>
-				Also viel Spaß beim stöbern!</br>
+				<br />
+				Hier findest du eine auflistung aller Charaktere, die von den Usern als öffentlich makiert wurden.<br />
+				Jeder Char hat seine eigene Seite, klicke einfach auf den Namen um dir einen Char genauer an zu schauen<br />
+				Wenn du eingeloggt bist, kriegst du sogar noch mehr Informationen. Denn neben der Liste die du hier siehst,<br />
+				bekommst du noch eine Liste deiner eigenen Chars und kannst diese modofizieren<p>
+				
+				Also viel Spaß beim stöbern!<p>
 
 			</article>';
 		break;
@@ -97,6 +96,20 @@ switch( $_GET["c"] ) {
 				</form>
 			</article>';
 		}
+		break;
+	case 4:		
+		echo '
+			<header><h1><span aria-hidden="true" class="icon-clipboard"></span> Charaktere</h1></header>
+			<article>
+				<br />
+				Hier findest du eine auflistung aller Charaktere, die von den Usern als öffentlich makiert wurden.<br />
+				Jeder Char hat seine eigene Seite, klicke einfach auf den Namen um dir einen Char genauer an zu schauen<br />
+				Wenn du eingeloggt bist, kriegst du sogar noch mehr Informationen. Denn neben der Liste die du hier siehst,<br />
+				bekommst du noch eine Liste deiner eigenen Chars und kannst diese modofizieren.<p>
+				
+				Also viel Spaß beim stöbern!<br />
+
+			</article>';
 		break;
 
 }
